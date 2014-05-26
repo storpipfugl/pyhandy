c0a728f7c3826e72f69abbd4ef7806002e7bed20
Esben S. Nielsen <storpipfuglmail.com>
2014-05-26 22:00:29 +0200

    Bumped bugfix version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyhandy',
-      version='0.1.0',
+      version='0.1.1',
       description='Python implementation of the HANDY model',
       author='Esben S. Nielsen',
       author_email='storpipfugl@gmail.com',
